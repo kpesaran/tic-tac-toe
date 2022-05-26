e03c7ab94b1f48411eb2935fe379e67c54c3e6a7
David Chen <davidchen94@outlook.com>
2022-05-25 20:29:01 -0700

    clean up script.js; modify dom directly; todo: use a state manager (aka. gameboardObject)

--- script.js
+++ script.js
@@ -1,98 +1,31 @@
-//Gameboard to keep tract of moves, once either player x or circle gets their symbols in three cells in a row they win.
-const gameboard = [
-    '', '', '',
-    '', '', '',
-    '', '', ''
-]
+const board = document.querySelector('.board');
+const cells = board.querySelectorAll('.cell');
 
-const gameboardObject = (newMark) => {
-   
-    if (circleTurn)
-        gameboard[newMark] = 'circle'
-    else if (!circleTurn)
-        gameboard[newMark] = 'x'
-    return {gameboard}
-}
-let board = document.querySelector('.board')
+const restartButton = document.getElementById('restart');
 
-let cell = board.querySelectorAll('.cell')
+let circleTurn = false;
 
-const circleTurn = false
+cells.forEach(cell => {
+  cell.addEventListener('click', e => {
+    const clickedCell = e.currentTarget;
 
-for (i = 0; i < cell.length; i++) {
-    cell[i].addEventListener('click', () => {
-        placeMark_swicthTurn()
-        return console.log('one of 9 cells should of been clicked')
-
-    })
-};
-
-function playGame() {
-    console.log('game started');
-    cell.addEventListener('click', () => {
-        console.log('cell got clicked')
-    });
-}
-
-//placeMark depending on whose turn it is will place mark and then switch turns 
-clickedCell = document.querySelector('.cell')
-clickedCell.addEventListener(
-    'click',
-    placeMark_swicthTurn(),
-    gameboardObject())
-
-
-function placeMark_swicthTurn() {
-    
-        if (!circleTurn) {
-            console.log('it is x turn this should mark x in the spot unless circle or x already there')
-            
-            if (clickedCell.classList.contains('circle')
-                ||
-                clickedCell.classList.contains('x')){
-                return console.log('nothing should happened still x\'s turn')
-            }
-            else {
-                clickedCell.classList.add('x')
-
-                circleTurn = true
-            }
-        }
-        else if (circleTurn) {
-            console.log('it is \'circles\' turn currently, a circle should mark in the cell clicked unless already x or o')  
-            if (clickedCell.classList.contains('circle') || clickedCell.classList.contains('x')) {
-                return console.log('nothing should happened still o\'s turn')
-            }
-            else {
-                clickedCell.classList.add('circle')
-                circleTurn = false
-            }
-        }
+    if (
+      clickedCell.classList.contains('circle') ||
+      clickedCell.classList.contains('x')
+    ) {
+      console.log("nothing should happened still x's turn");
+      return;
     }
-;
- 
 
+    clickedCell.classList.add(circleTurn ? 'circle' : 'x');
 
-function restartGame() {
+    circleTurn = !circleTurn;
+  });
+});
 
-    //reset the gameboard of array again as empty string for all nine elements 
-    for (i = 0; i < gameboardObject.length; i++) {
-        
-        gameboard[i] = ''
-    }
-    //adjust html to remove x and circle classes to remove marks     
-    if (cell.classList.contains('x')) {
-         cell.classList.remove('x')
-        }
-    if (cell.classList.contains('circle')){
-         cell.classList.remove('circle')
-    }
-};
-
-let restartButton = document.getElementById('restart')
-restartButton.addEventListener('click', () => restartGame())
-
-
-
-
-
+restartButton.addEventListener('click', () => {
+  cells.forEach(cell => {
+    cell.classList.remove('x');
+    cell.classList.remove('circle');
+  });
+});
