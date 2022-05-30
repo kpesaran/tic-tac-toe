1010c79b4ee0a5d1c06b7e2256fe4ac470cddc5a
David Chen <davidchen94@outlook.com>
2022-05-29 22:21:41 -0700

    add internal game state; todo: sync game state with dom

--- script.js
+++ script.js
@@ -3,6 +3,125 @@ const cells = board.querySelectorAll('.cell');
 
 const restartButton = document.getElementById('restart');
 
+class Player {
+  constructor(role) {
+    this.role = role;
+  }
+
+  move() {
+    // todo: return [x, y]
+    // hint: get imput from event listener
+  }
+}
+
+class GameState {
+  constructor() {
+    this.restart();
+  }
+
+  restart() {
+    this.board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ];
+    this.player1 = new Player('x');
+    this.player2 = new Player('o');
+  }
+
+  play() {
+    let currPlayer = this.player1;
+    let prevPlayer = this.player2;
+
+    while (!this.checkWinner()) {
+      let currMove = currPlayer.move();
+      let x = currMove[0],
+        y = currMove[1];
+
+      // todo: check if currMove is valid
+
+      this.board[y][x] = currPlayer.role;
+
+      this.updateDOM();
+
+      let tmpPlayer = currPlayer;
+      currPlayer = prevPlayer;
+      prevPlayer = tmpPlayer;
+    }
+  }
+
+  updateDOM() {
+    for (y = 0; y < 3; ++y) {
+      for (x = 0; x < 3; ++x) {
+        let cellsIdx = y * 3 + x;
+
+        cells[cellsIdx].classList.remove('x');
+        cells[cellsIdx].classList.remove('circle');
+
+        if (this.board[y][x] != '')
+          cells[cellsIdx].classList.add(this.board[y][x]);
+      }
+    }
+  }
+
+  validateMove(x, y) {
+    // todo
+    // return true if a move is valid
+  }
+
+  // todo: give this function a better name
+  isValidWinningState(cells) {
+    // len(cells) = 3
+    return cells[0] != '' && cells[0] === cells[1] && cells[1] === cells[2];
+  }
+
+  checkWinner() {
+    // todo: check draw
+
+    for (y = 0; y < 3; ++y)
+      if (this.isValidWinningState(this.board[y])) return this.board[y][0];
+
+    for (x = 0; x < 3; ++x) {
+      if (
+        this.isValidWinningState([
+          this.board[0][x],
+          this.board[1][x],
+          this.board[2][x]
+        ])
+      ) {
+        return this.board[0][x];
+      }
+    }
+
+    if (
+      this.isValidWinningState(
+        this.board[0][0],
+        this.board[1][1],
+        this.board[2][2]
+      )
+    ) {
+      return this.board[0][0];
+    }
+
+    if (
+      this.isValidWinningState(
+        this.board[0][2],
+        this.board[1][1],
+        this.board[2][0]
+      )
+    ) {
+      return this.board[0][2];
+    }
+  }
+}
+
+/* ---------- old code ----------
+
+const board = document.querySelector('.board');
+const cells = board.querySelectorAll('.cell');
+
+const restartButton = document.getElementById('restart');
+
 let circleTurn = false;
 
 cells.forEach(cell => {
@@ -29,3 +148,4 @@ restartButton.addEventListener('click', () => {
     cell.classList.remove('circle');
   });
 });
+*/
